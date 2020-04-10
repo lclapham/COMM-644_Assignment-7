1bc0aa3958beb276a5ffb4d4e872e2cb2fa659f6
Karen Kuzmiak <lamar.clapham@gmail.com>
2020-04-09 19:18:26 -0700

    Part 1 Step 2 completed

--- js/strings.js
+++ js/strings.js
@@ -1,13 +1,20 @@
 /*eslint-env browser*/
 
 //STEP 1
-function nameLength() {
-    let name = window.prompt("Please input your name");
-    window.alert("There are " +name.length+ " letters in your name.")
-    
-}
+// function nameLength() {
+//     let name = window.prompt("Please input your name");
+//     window.alert("There are " +name.length+ " letters in your name.")
+
+// }
 
 //STEP 2
+
+function findLetter() {
+    let name = window.prompt("Please input your name");
+    let number = window.prompt("Enter a number between 1 and " + name.length);
+    window.alert(number + " letter(s) into your name is " + name[number - 1]);
+
+}
 
 //STEP 3
 
