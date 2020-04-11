703919a4d3dc1900b8bb26261b346835cd637a06
Karen Kuzmiak <lamar.clapham@gmail.com>
2020-04-10 20:51:47 -0700

    part 2 Step 3 completed

--- js/math.js
+++ js/math.js
@@ -2,13 +2,23 @@
 
 //STEP 1
 function positiveNumber() {
-    let number = window.prompt("Enter a positive or negative number")
+    let number = window.prompt("Enter a positive or negative number");
     console.log(Math.abs(number));
 }
 
 //STEP 2
+function roundUP() {
+    let floater = window.prompt("Enter a number as a decimal");
+    console.log(Math.ceil(floater));
+    
+}
 
 //STEP 3
+function roundUP() {
+    let floater = window.prompt("Enter a number as a decimal");
+    console.log(Math.floor(floater));
+    
+}
 
 //STEP 4
 
