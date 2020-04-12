b686a6f977d522be5ec46057b7118d09d8ca94e0
Karen Kuzmiak <lamar.clapham@gmail.com>
2020-04-11 18:21:21 -0700

    Part 3 Step 1 Completed

--- js/dates.js
+++ js/dates.js
@@ -1,6 +1,15 @@
 /*eslint-env browser*/
 
 //STEP 1
+let year =  window.prompt("Enter the year (i.e. 2020");
+let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+let days;
+
+for(let i = 0; i < 12; i++){
+    days = new Date(year, [i + 1], 0).getDate();
+    console.log("In the year " + year + " There are "+ days + " days in " + months[i])
+
+}
 
 //STEP 2
 
