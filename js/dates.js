a94af606093adf1e3c31f7af91b9866aa2dc0be9
Karen Kuzmiak <lamar.clapham@gmail.com>
2020-04-11 21:07:53 -0700

    Part 3 Step 4 Completed

--- js/dates.js
+++ js/dates.js
@@ -4,25 +4,27 @@
 const d = new Date();
 
 //STEP 1
-let year =  window.prompt("Enter the year (i.e. 2020", 2049);
+let year = window.prompt("Enter the year (i.e. 2020", 2049);
 let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 let days;
 
-for(let i = 0; i < 12; i++){
+console.log("Step 1");
+for (let i = 0; i < 12; i++) {
     days = new Date(year, [i + 1], 0).getDate();
-    console.log("In the year " + year + " There are "+ days + " days in " + months[i])
+    console.log("In the year " + year + " There are " + days + " days in " + months[i])
 
 }
 
 //STEP 2
-let userDate = window.prompt("Enter a date (January 3, 2021)", "January 3, 2021");
+let userDate = window.prompt("Enter a date (January 3, 2021)", d);
 let d1 = new Date(userDate);
+console.log("Step 2");
 console.log(d1.toLocaleString('default', { month: 'long' }));
 
 //STEP 3
- userDate = window.prompt("Enter a date (April 11, 2020)", "April 11, 2020");
+userDate = window.prompt("Enter a date (April 11, 2020)", d);
 let date = new Date(userDate);
-
+console.log("Step 3");
 switch (date.getDay()) {
     case 0:
         day = "Sunday";
@@ -55,5 +57,10 @@ switch (date.getDay()) {
 }
 
 //STEP 4
+userDate = window.prompt("Enter a date (April 11, 2020)", d);
+let convertDate = (new Date(userDate).getDay() - 1);
+let dayArr = ['Sunday','Monday','Tuesday','Wednesday', 'Thursday','Friday', 'Saturday']
+console.log("Step 4");
+console.log("Yesterday was " + dayArr[convertDate]);
 
 //STEP 5
